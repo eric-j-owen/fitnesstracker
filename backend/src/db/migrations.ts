1e0e9d958aa0e5e0828b5ba1558568bdfb3ecc07
eric-j-owen <ejowen+github@proton.me>
2025-03-26 13:18:54 -0400

    adds additional pg tables

--- backend/src/db/migrations.ts
+++ backend/src/db/migrations.ts
@@ -49,4 +49,67 @@ migrationsUp.set(
     );`
 );
 
+migrationsUp.set(
+  "create-metrics-table",
+  `create table if not exists metrics (
+      id serial primary key,
+      user_id int not null references users(id) on delete cascade,
+      type text not null check (type in ('weight')),
+      value DECIMAL(8,2) not null,
+      date date not null default current_date,
+      unique (user_id, date, type)
+  );`
+);
+
+migrationsUp.set(
+  "create-nutrition-table",
+  `create table if not exists nutrition (
+      id serial primary key,
+      user_id int not null references users(id) on delete cascade,
+      calories int not null,
+      protein int not null,
+      carbs int not null,
+      fats int not null,
+      date date not null default current_date,
+      unique (user_id, date)
+  );`
+);
+
+migrationsUp.set(
+  "create-exercises-table",
+  `create table if not exists exercises (
+      id serial primary key,
+      user_id int not null references users(id) on delete cascade,
+      exercise text not null,
+      unique(user_id, exercise)
+    );`
+);
+
+migrationsUp.set(
+  "create-workouts-table",
+  `create table if not exists workouts (
+      id serial primary key,
+      user_id int references users(id) on delete cascade,
+      exercise_id int references exercises(id) on delete cascade,
+      sets int not null check (sets > 0),
+      reps int not null check (reps > 0),
+      weight decimal(6,2) not null,
+      created_at timestamptz default now()
+    );`
+);
+
+migrationsUp.set(
+  "create-targets-table",
+  `create table if not exists targets (
+      id serial primary key,
+      user_id int not null references users(id) on delete cascade,
+      target_type text not null check (target_type in ('macro', 'metric', 'exercise')),
+      exercise_id int references exercises(id) on delete cascade,
+      macro_type text check (macro_type in ('calories', 'protein', 'carbs', 'fats')),
+      metric_type text check (metric_type in ('weight')),
+      target_value decimal(5, 2) not null
+    );`
+);
+
+
 if (import.meta.url === `file://${process.argv[1]}`) runMigrationsAll();
