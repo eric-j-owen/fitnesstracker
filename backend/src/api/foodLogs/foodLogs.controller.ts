4c5d515157da0944259ace1fc3190ffeae3f5397
eric-j-owen <ejowen+github@proton.me>
2025-07-16 13:46:37 -0400

    starts create food log controller/route

--- backend/src/api/foodLogs/foodLogs.controller.ts
+++ backend/src/api/foodLogs/foodLogs.controller.ts
@@ -2,17 +2,37 @@ import type { RequestHandler } from "express";
 import AppDataSource from "../../db/data-source.js";
 import { FoodItem } from "../../db/entities/foodItem.entity.js";
 import { FoodLog } from "../../db/entities/foodLog.entity.js";
+import type { FoodLogReqBody } from "../api.types.js";
 
 const foodLogRepo = AppDataSource.getRepository(FoodLog);
 const foodItemRepo = AppDataSource.getRepository(FoodItem);
 
-export const createLogEntry: RequestHandler = async (req, res, next) => {
-  const { userId } = req.session;
-  const { fdcId } = req.body;
+export const getFoodLogEntry: RequestHandler = async (req, res, next) => {};
+export const getFoodLogDay: RequestHandler = async (req, res, next) => {};
 
+export const createLogEntry: RequestHandler<
+  unknown,
+  unknown,
+  FoodLogReqBody,
+  unknown
+> = async (req, res, next) => {
   try {
+    const { userId } = req.session;
+    const {
+      fdcId,
+      mealCategory,
+      amount,
+      unit,
+      logDate,
+      calculatedCalories,
+      calculatedProtein,
+      calculatedCarbs,
+      calculatedFat,
+    } = req.body;
   } catch (err) {
     next(err);
   }
   const foodItem = await foodItemRepo.findOneBy({});
 };
+
+export const editLogEntry: RequestHandler = async (req, res, next) => {};
--- backend/src/api/foodLogs/foodLogs.routes.ts
+++ backend/src/api/foodLogs/foodLogs.routes.ts
@@ -5,13 +5,19 @@ import { createFoodLogSchema } from "../api.schemas.js";
 
 const router = Router();
 
+// GET /api/foodlog/:id
+// GET /api/foodlog
 // POST  /api/Foodlog
+// PATCH /api/foodlog/:id
 
+router.get("/", Controller.getFoodLogDay);
+router.get("/:id", Controller.getFoodLogEntry);
 router.post(
   "/",
 
   validate(createFoodLogSchema),
   Controller.createLogEntry
 );
+router.patch("/:id", Controller.editLogEntry);
 
 export default router;
