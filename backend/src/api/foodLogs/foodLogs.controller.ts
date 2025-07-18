d892b51f9f4ebdf7a4614c1912d948d5c2e5bcf2
eric-j-owen <ejowen+github@proton.me>
2025-07-18 15:41:05 -0400

    adds basic logic for logging food

--- backend/src/api/foodLogs/foodLogs.controller.ts
+++ backend/src/api/foodLogs/foodLogs.controller.ts
@@ -1,14 +1,16 @@
 import type { RequestHandler } from "express";
 import AppDataSource from "../../db/data-source.js";
 import { FoodItem } from "../../db/entities/foodItem.entity.js";
-import { FoodLog } from "../../db/entities/foodLog.entity.js";
+import { FoodLog, MealCategory } from "../../db/entities/foodLog.entity.js";
 import type { FoodLogReqBody } from "../api.types.js";
 
 const foodLogRepo = AppDataSource.getRepository(FoodLog);
 const foodItemRepo = AppDataSource.getRepository(FoodItem);
 
-export const getFoodLogEntry: RequestHandler = async (req, res, next) => {};
-export const getFoodLogDay: RequestHandler = async (req, res, next) => {};
+export const getLogEntry: RequestHandler = async (req, res, next) => {};
+export const getLoggedDay: RequestHandler = async (req, res, next) => {};
+export const deleteLogEntry: RequestHandler = async (req, res, next) => {};
+export const editLogEntry: RequestHandler = async (req, res, next) => {};
 
 export const createLogEntry: RequestHandler<
   unknown,
@@ -29,10 +31,28 @@ export const createLogEntry: RequestHandler<
       calculatedCarbs,
       calculatedFat,
     } = req.body;
+
+    const foodItem = await foodItemRepo.findOneBy({ fdcId });
+    if (!foodItem) {
+      return res.status(404).json({ message: "food item not found" });
+    }
+
+    const logEntry = foodLogRepo.create({
+      user: { id: userId },
+      foodItem,
+      mealCategory: mealCategory as MealCategory,
+      amount,
+      unit,
+      logDate: new Date(logDate),
+      calculatedCalories,
+      calculatedProtein,
+      calculatedCarbs,
+      calculatedFat,
+    });
+
+    const saved = await foodLogRepo.save(logEntry);
+    res.status(201).json(saved);
   } catch (err) {
     next(err);
   }
-  const foodItem = await foodItemRepo.findOneBy({});
 };
-
-export const editLogEntry: RequestHandler = async (req, res, next) => {};
